407c8143c5250bb7c625a67666aed4fe23df197d
Estradita05 <124050385@upq.edu.mx>
2025-11-13 18:48:39 -0600

    Modal Completada

---
+++ intro/screens/ModalScreens.js
@@ -0,0 +1,10 @@
+import { Text, StyleSheet, View } from 'react-native'
+
+export default function ModalScreen () {
+  return (
+    <View>
+      <Text>Proximamente by Rogelio & equipo</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({})
