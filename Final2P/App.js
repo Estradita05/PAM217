b8ee347981c9eada046bf6cfbf41f5f1e9cf74fe
Estradita05 <124050385@upq.edu.mx>
2025-12-08 11:07:34 -0600

    Actualización de proyecto

---
+++ Final2P/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Examen final 2do Parcial</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
