491825211f9d0b645a4beb77411a557c23c152cc
Estradita05 <124050385@upq.edu.mx>
2025-12-08 17:13:24 -0600

    Actualicé estas screens

--- Final2P/App.js
+++ Final2P/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Examen final 2do Parcial</Text>
+      <Text>Examen final 2do Parcial 08/12/2025</Text>
       <StatusBar style="auto" />
     </View>
   );
